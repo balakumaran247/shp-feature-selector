15bb109030c40a63f8d344483d8c7381fcd60d65
Balakumaran <balakumaran247@gmail.com>
2022-12-08 23:01:51 +0530

    updated: refactor filter exp, method 4 unique_vals

--- feature_selector_core.py
+++ feature_selector_core.py
@@ -25,34 +25,41 @@ class LayerHandler:
             layer = QgsVectorLayer(filepath, filename[0], "ogr")
             self.instance.addMapLayer(layer)
 
+    def get_col_names(self, layer):
+        return layer.fields().names()
+
     def check_layer(self, layer):
-        field_names = layer.fields().names()
+        field_names = self.get_col_names(layer)
         return (all((name in field_names for name in self.jaltol_cols)))
 
     def get_layer(self, lyr_name):
-        if layers:
-            = self.instance.mapLayersByName(lyr_name):
+        if layers:= self.instance.mapLayersByName(lyr_name):
             return layers[0]
 
+    def get_unique_values(self, layer, col_name):
+        idx = layer.fields().indexOf(col_name)
+        return sorted(map(lambda x: str(x), filter(lambda y: isinstance(y, (str, int, float)), layer.uniqueValues(idx))))
+
     def get_state_list(self, layer):
-        idx = layer.fields().indexOf(self.jaltol_cols[0])
-        return sorted(filter(lambda x: isinstance(x, str), layer.uniqueValues(idx)))
+        return self.get_unique_values(layer, self.jaltol_cols[0])
 
     def get_dist_list(self, layer, state):
-        stateFilter = f"\"{self.jaltol_cols[0]}\"='{state}'"
+        stateFilter = self._get_filter_exp((self.jaltol_cols[0], state))
         return self._extract_info(stateFilter, layer, self.jaltol_cols[1])
 
     def get_block_list(self, layer, state, dist):
-        stateFilter = f"\"{self.jaltol_cols[0]}\"='{state}'"
-        distFilter = f"\"{self.jaltol_cols[1]}\"='{dist}'"
-        vlayerFilter = f'{stateFilter} and {distFilter}'
+        vlayerFilter = self._get_filter_exp(
+            (self.jaltol_cols[0], state),
+            (self.jaltol_cols[1], dist)
+        )
         return self._extract_info(vlayerFilter, layer, self.jaltol_cols[2])
 
     def get_village_list(self, layer, state, dist, block):
-        stateFilter = f"\"{self.jaltol_cols[0]}\"='{state}'"
-        distFilter = f"\"{self.jaltol_cols[1]}\"='{dist}'"
-        blockFilter = f"\"{self.jaltol_cols[2]}\"='{block}'"
-        vlayerFilter = f'{stateFilter} and {distFilter} and {blockFilter}'
+        vlayerFilter = self._get_filter_exp(
+            (self.jaltol_cols[0], state),
+            (self.jaltol_cols[1], dist),
+            (self.jaltol_cols[2], block)
+        )
         return self._extract_info(vlayerFilter, layer, self.jaltol_cols[3])
 
     def _extract_info(self, vlayerFilter, layer, col_name):
@@ -60,3 +67,6 @@ class LayerHandler:
         stateFeas = layer.getFeatures(QgsFeatureRequest(expr))
         li = [fea[col_name] for fea in stateFeas if fea[col_name]]
         return sorted(set(filter(lambda x: isinstance(x, str), li)))
+
+    def _get_filter_exp(self, *args):
+        return ' and '.join(map(lambda x: f"\"{x[0]}\"='{x[1]}'", args))
